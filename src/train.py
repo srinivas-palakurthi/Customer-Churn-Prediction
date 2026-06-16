8ec391f17dd1928830f173d859f939fbdf6e28eb
srinivas-palakurthi <130201966+srinivas-palakurthi@users.noreply.github.com>
2026-06-16 17:39:44 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("data/churn.csv")
+
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+X = pd.get_dummies(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+print(
+    "Accuracy:",
+    accuracy_score(y_test, predictions)
+)
+
+joblib.dump(model, "model.pkl")
